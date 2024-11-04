3ddc427d06c867874f0055cc30cb5400f1cb5ad2
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-11-04 14:11:56 +0000

    Display cart items

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,8 @@
 import carbonNeutralIcon from "../assets/images/icon-carbon-neutral.svg";
 import { ICart } from "../types/cart";
+import { IDessert } from "../types/dessert";
+
+import removeItemIcon from "../assets/images/icon-remove-item.svg";
 
 interface CartProps {
   cart: ICart;
@@ -20,14 +23,48 @@ function Cart({ cart }: CartProps) {
       totalPrice += cart[item].reduce((acc, curr) => acc + curr.price, 0);
     }
 
-    return totalPrice;
+    return totalPrice.toFixed(2);
+  }
+
+  function getItemTotal(item: IDessert[]) {
+    return item.reduce((acc, curr) => acc + curr.price, 0).toFixed(2);
   }
 
   return (
     <section className="rounded-2xl bg-white p-4">
       <p className="text-orange-700 font-bold">Your Cart ({getCartLength()})</p>
       <div className="my-4">
-        <div>{/* cart items */}</div>
+        <div>
+          {Object.keys(cart).map((itemName, idx) => {
+            return (
+              <div key={idx}>
+                <div className="flex items-center">
+                  <div className="text-xs">
+                    <h4 className="text-yellow-950  font-semibold">
+                      {itemName}
+                    </h4>
+                    <h6 className="flex">
+                      <span className="text-orange-700 font-semibold mr-3">
+                        {cart[itemName].length}x
+                      </span>{" "}
+                      <span className="text-stone-400 mr-3">
+                        @ ${cart[itemName][0].price.toFixed(2)}
+                      </span>{" "}
+                      <span className="text-stone-400 font-semibold">
+                        ${getItemTotal(cart[itemName])}
+                      </span>
+                    </h6>
+                  </div>
+
+                  <button className="ml-auto">
+                    <img src={removeItemIcon} />
+                  </button>
+                </div>
+                <hr className="my-4"></hr>
+              </div>
+            );
+          })}
+        </div>
         <div className="flex items-center">
           <span className="text-xs">Order Total</span>{" "}
           <span className="ml-auto font-bold text-lg">${getCartTotal()}</span>
